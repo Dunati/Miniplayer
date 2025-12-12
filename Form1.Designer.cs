e55cf064c47354f35b26df133030af6bc180908e
Dolphin Hawkins <dhawkins2@anduril.com>
2025-12-11 19:03:28 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+namespace MiniPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(34, 64, 153);
+            ClientSize = new Size(148, 100);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Padding = new Padding(5);
+            Text = "MiniPlayer";
+            MouseDoubleClick += Form1_MouseDoubleClick;
+            ResumeLayout(false);
+
+
+        }
+
+        #endregion
+
+    }
+}
